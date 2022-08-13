389635a779c75e8a54adf42136791925920e173e
SHALFY007 <70435162+SHALFY007@users.noreply.github.com>
2022-08-14 00:15:46 +0300

    Add script

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const goods = [
+{ title: 'Shirt', price: 150 },
+{ title: 'Socks', price: 50 },
+{ title: 'Jacket', price: 350 },
+{ title: 'Shoes', price: 250 },
+];
+const renderGoodsItem = (title, price) => {
+    return `<div class="good-card">
+                   <h3 class="good-card-header">${title}</h3>
+                   <p class="good-card-price">${price}</p>
+                   <a href="#" class="good-card-button">Purchase</a>
+               </div>`;
+};
+const renderGoodsList = (list) => {
+let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join('');
+    document.querySelector('.goods-list').innerHTML = goodsList;
+}
+renderGoodsList(goods);
